848c992b1f3dd79213eac387ac08c3906b3dc0b9
Cristian Monroy <monroytupai@gmail.com>
2023-07-31 21:16:48 -0600

    pagina de inicio

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import App_cristian from './App-cristian.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <App_cristian />
   </React.StrictMode>,
 )
